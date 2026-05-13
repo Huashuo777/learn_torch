771a16ff37dc335eed01532a7fdc2a57d1341988
Kup <hhhaaa786@163.com>
2026-05-13 17:20:51 +0800

    Update lecture.py

--- Back_Propagation/lecture.py
+++ Back_Propagation/lecture.py
@@ -2,15 +2,19 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-x_data = np.array([1.0,2.0,3.0])
-y_data = np.array([2.0,4.0,6.0])
+x_data = [1.0,2.0,3.0]
+y_data = [2.0,4.0,6.0]
 
-w = torch.Tensor([1.0])
-w.required_grad = True
+w1 = torch.Tensor([1.0])
+w2 = torch.Tensor([2.0])
+b = torch.Tensor([1.0]) 
+w1.requires_grad = True
+w2.requires_grad = True
+b.requires_grad = True
 lr = 0.1 
 
 def forward(x):
-    return x * w
+    return x**2 * w1 + x * w2 + b
 
 def loss(x, y):
     y_pred = forward(x)
@@ -19,12 +23,17 @@ def loss(x, y):
 print("predict(before training)", 4, forward(4))
 
 for epoch in range(100):
-    for i in x_data.shape[0]:
+    for i in range(len(x_data)):
         l = loss(x_data[i], y_data[i])
         l.backward()
-        print('\tgrad:', x_data[i], y_data[i], w.grad.item())
-        w.data = w.data - w.grad.data * lr
+        print('\tgrad:', x_data[i], y_data[i], w1.grad.item(),w2.grad.item(),b.grad.item())
+        w1.data = w1.data - w1.grad.data * lr
+        w2.data = w2.data - w2.grad.data * lr
+        b.data = b.data - b.grad.data * lr
 
-        w.grad.data.zero_()
+        w1.grad.data.zero_()
+        w2.grad.data.zero_()
+        b.grad.data.zero_()
     print("progress:",epoch, l.item())
 print('now,predict:',forward(4))
+
